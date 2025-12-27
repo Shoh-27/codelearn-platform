b962e0cb1108ac1be3bedf83f5ad94b073126c15
Shohrux <shohruxnizomov170@gmail.com>
2025-12-27 23:13:14 +0500

    lesson modeli fillable va casts bilan qo‘shildi

---
+++ backend/app/Models/Lesson.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Lesson extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'content',
+        'difficulty',
+        'xp_reward',
+        'order_index',
+        'is_published',
+    ];
+
+    protected $casts = [
+        'xp_reward' => 'integer',
+        'order_index' => 'integer',
+        'is_published' => 'boolean',
+    ];
+
+}
