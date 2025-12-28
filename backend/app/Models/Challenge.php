ad00abc72db9025dd1ffc2c45ce8ae355f7be809
Shohrux <shohruxnizomov170@gmail.com>
2025-12-28 15:51:12 +0500

    ChTshiriq sarlavhasidan slug generatsiyasi qo‘shildi

--- backend/app/Models/Challenge.php
+++ backend/app/Models/Challenge.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Challenge extends Model
 {
@@ -34,6 +35,15 @@ class Challenge extends Model
         'is_published' => 'boolean',
     ];
 
+    protected static function boot()
+    {
+        parent::boot();
 
+        static::creating(function ($challenge) {
+            if (empty($challenge->slug)) {
+                $challenge->slug = Str::slug($challenge->title);
+            }
+        });
+    }
 }
 
