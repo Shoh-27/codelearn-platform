0e0a3ec93b7e78f2d5bc99fbb4daf133d30426f1
Shohrux <shohruxnizomov170@gmail.com>
2025-12-28 16:29:39 +0500

    ChallengeController service qatlamiga ulandi

--- backend/app/Http/Controllers/ChallengeController.php
+++ backend/app/Http/Controllers/ChallengeController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\LessonService;
+use App\Services\ChallengeService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ChallengeController extends Controller
 {
-    //
+    private ChallengeService $challengeService;
+
+    public function __construct(ChallengeService $challengeService)
+    {
+        $this->challengeService = $challengeService;
+    }
+
+
 }
